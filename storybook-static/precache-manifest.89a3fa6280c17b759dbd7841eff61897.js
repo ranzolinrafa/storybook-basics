c46fa1bc78cfcb27d418dea874fa3b9b11dffb83
rafaelpedroso <rafael.pedroso@compasso.com.br>
2020-10-01 02:16:05 -0300

    First commit

---
+++ storybook-static/precache-manifest.89a3fa6280c17b759dbd7841eff61897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d704b4ce71cd5368f1b21e328d6a6758",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.40a281c2808cb3477a14.bundle.js"
+  },
+  {
+    "url": "runtime~main.40a281c2808cb3477a14.bundle.js"
+  },
+  {
+    "revision": "4a3ab6cae253cf71a35b",
+    "url": "static/css/main.b15adadd.chunk.css"
+  },
+  {
+    "url": "vendors~main.40a281c2808cb3477a14.bundle.js"
+  },
+  {
+    "url": "vendors~main.40a281c2808cb3477a14.bundle.js.LICENSE.txt"
+  }
+]);
